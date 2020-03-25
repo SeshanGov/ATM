29bb039af09ac26725d04ca1b24d063ea6fd0d54
Seshan Govender <seshan.govender@gmail.com>
2020-03-25 13:43:18 +0200

    Adds method to search for user and unit tests for same

--- src/ATM_Simulation.java
+++ src/ATM_Simulation.java
@@ -1,7 +1,16 @@
 public class ATM_Simulation {
 	
+	String[] userNames = {"seshan.govender@gmail.com"};
+	
 	public static void main(String[] args) {
 		System.out.println("Test");
+	}
+
+	public String findUserName(String userInput, String[] listOfUserNames) {
+		for (String userName: listOfUserNames) {
+			if (userName.equalsIgnoreCase(userInput)) return userInput;
+		}
+		return "User not found";		
 	}
 
 }
--- test/ATMSimulationTests.java
+++ test/ATMSimulationTests.java
@@ -5,8 +5,24 @@ import org.junit.Test;
 public class ATMSimulationTests {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testCanFindUserNameRegardlessOfCaseSensitivity() {
+		ATM_Simulation testObject = new ATM_Simulation();
+		String desiredOutcome = "seshan.govender@gmail.com";
+		String result = testObject.findUserName(desiredOutcome, testObject.userNames);
+		assertEquals("First test:", desiredOutcome, result);
+	
+		desiredOutcome = "Seshan.Govender@Gmail.Com";
+		result = testObject.findUserName(desiredOutcome, testObject.userNames);
+		assertEquals("Second test:", desiredOutcome, result);
+	}
+	
+	@Test
+	public void testCannotFindUserNameInDatabase() {
+		ATM_Simulation testObject = new ATM_Simulation();
+		String userInput = "seshan.govender@gmail.co.za";
+		String desiredOutcome = "User not found";
+		String result = testObject.findUserName(userInput, testObject.userNames);
+		assertEquals(desiredOutcome, result);
 	}
 
 }
