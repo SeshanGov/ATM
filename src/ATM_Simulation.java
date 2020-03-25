dc6ef1b8861895eda0e02ecbe33cc1d152309b61
Seshan Govender <seshan.govender@gmail.com>
2020-03-25 13:53:19 +0200

    Adds business logic for retrieving valid password and unit test for same

--- src/ATM_Simulation.java
+++ src/ATM_Simulation.java
@@ -1,6 +1,7 @@
 public class ATM_Simulation {
 	
 	String[] userNames = {"seshan.govender@gmail.com"};
+	String[] passwords = {"myPa$$word"};
 	
 	public static void main(String[] args) {
 		System.out.println("Test");
@@ -11,6 +12,13 @@ public class ATM_Simulation {
 			if (userName.equalsIgnoreCase(userInput)) return userInput;
 		}
 		return "User not found";		
+	}
+
+	public String locatePassword(String userInput, String[] listOfPasswords) {
+		for (String password : listOfPasswords) {
+			if (password.equals(userInput)) return userInput;
+		}
+		return null;
 	}
 
 }
--- test/ATMSimulationTests.java
+++ test/ATMSimulationTests.java
@@ -1,12 +1,20 @@
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class ATMSimulationTests {
+	
+	ATM_Simulation testObject;
+	
+	@Before
+	public void setup() {
+		testObject = new ATM_Simulation();
+	}
 
 	@Test
 	public void testCanFindUserNameRegardlessOfCaseSensitivity() {
-		ATM_Simulation testObject = new ATM_Simulation();
+		
 		String desiredOutcome = "seshan.govender@gmail.com";
 		String result = testObject.findUserName(desiredOutcome, testObject.userNames);
 		assertEquals("First test:", desiredOutcome, result);
@@ -14,14 +22,25 @@ public class ATMSimulationTests {
 		desiredOutcome = "Seshan.Govender@Gmail.Com";
 		result = testObject.findUserName(desiredOutcome, testObject.userNames);
 		assertEquals("Second test:", desiredOutcome, result);
+		
 	}
 	
 	@Test
 	public void testCannotFindUserNameInDatabase() {
-		ATM_Simulation testObject = new ATM_Simulation();
+		
 		String userInput = "seshan.govender@gmail.co.za";
 		String desiredOutcome = "User not found";
 		String result = testObject.findUserName(userInput, testObject.userNames);
+		assertEquals(desiredOutcome, result);
+		
+	}
+	
+	@Test
+	public void testCanFindPasswordInDatabase() {
+		testObject = new ATM_Simulation();
+		String desiredOutcome = "myPa$$word";
+		String result = testObject.locatePassword(desiredOutcome, testObject.passwords);
+		
 		assertEquals(desiredOutcome, result);
 	}
 
